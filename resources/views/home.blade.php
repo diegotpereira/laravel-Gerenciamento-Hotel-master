4055528c00da17b30f2ed0a43f94d701247304a4
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-06-24 13:36:47 -0300

    criação do componente home.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layout')
+
+@section('content')
+   <h1 style="text-align: center">Página Principal</h1>
+
+   <div class="container-fluid">
+       <div class="container">
+          <div class="col-md-6">
+          </div>
+
+          <div class="col-md-6">
+          </div>
+       </div>
+   </div>
+@endsection
